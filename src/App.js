3c438a9648140db9e7d24c975d5849b45c614c25
Jigar Shah <shahjigar185@gmail.com>
2022-07-10 13:39:23 +0530

    local storage caching

--- src/App.js
+++ src/App.js
@@ -3,29 +3,64 @@ import Header from "./components/Header";
 import countriesData from "./data/countries";
 import styles from "./App.module.css";
 import Table from "./components/Table";
-import { useState, useEffect } from "react";
+import { useState, useEffect ,useRef} from "react";
 import debounce from "lodash/debounce";
 import Loader from "./components/Loader";
 
+
+// error management
 function App() {
   const [countries, setCountries] = React.useState([...countriesData]);
   const [trending, setTrending] = useState([...countriesData]);
   const [activeTrending, setActiveTrending] = useState(true);
   const [text, setText] = useState("");
   const [loading, setLoading] = useState(false);
+  const [err,setErr]=useState(false);
+  
+
+  const lastAbortController = useRef()
 
   const getData = (val) => {
+    if (lastAbortController.current) {
+      lastAbortController.current.abort();
+    }
     console.log(`Fetching Data:${val}`);
-    setLoading(true);
+    const currentAbortController = new AbortController();
+    lastAbortController.current = currentAbortController;
+
+    // const currentPromise = fetch(id, {
+    //   signal: currentAbortController.signal,
+    // }).then(async data => {
+    //   return data;
+    // });
+
+    // currentPromise.then(
+    //   result => setData(result),
+    // ).catch(e)=> setErr(true);
+
     // imitate backend
-    setTimeout(() => {
-      const data = countries.filter(
-        (c) => c.name.toLowerCase().includes(val.toLowerCase()) == true
-      );
-      setCountries(data);
-      setLoading(false);
-    }, 1000);
+    try {
+      let data=JSON.parse(localStorage.getItem(val));
+      if(data){
+        setLoading(false);
+        setCountries(data);
+        return ;
+      }
+      setTimeout(() => {
+        const data = countries.filter(
+          (c) => c.name.toLowerCase().includes(val.toLowerCase()) == true
+        );
+        setCountries(data);
+        localStorage.setItem(val,JSON.stringify(data));
+        setLoading(false);
+      }, 1000);
+      
+    } catch (error) {
+      setErr(true);
+    }
   };
+
+ 
   const delayedHandleChange = React.useRef(
     debounce((eventData) => getData(eventData), 1000)
   ).current;
@@ -34,6 +69,7 @@ function App() {
     setText(val);
     if (val.length >= 3) {
       setActiveTrending(false);
+      setLoading(true)
       delayedHandleChange(val);
     } else {
       setActiveTrending(true);
